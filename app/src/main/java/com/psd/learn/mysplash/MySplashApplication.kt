71df2f2539454b92a494f9dcc1ee76699601f0d8
ducsang.pham <ducsang.pham55@gmail.com>
2024-05-20 01:21:49 +0700

    add logger classes

--- app/src/main/java/com/psd/learn/mysplash/MySplashApplication.kt
+++ app/src/main/java/com/psd/learn/mysplash/MySplashApplication.kt
@@ -1,11 +1,14 @@
 package com.psd.learn.mysplash
 
 import android.app.Application
+import com.psd.learn.mysplash.utils.log.LogcatLogWriter
+import com.psd.learn.mysplash.utils.log.Logger
 
 class MySplashApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         ServiceLocator.initWith(this)
+        Logger.init(LogcatLogWriter())
     }
 }
